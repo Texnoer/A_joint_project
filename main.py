26a5843d4d46e3dd108968b1f648379522174633
ReVadim <revani@yandex.ru>
2021-01-07 23:16:56 +0300

    добавил некоторые обьяснения в самом тексте программы

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ students = []
 courses = []
 all_lecturer = []
 
+
 class Student:
     def __init__(self, name, surname, gender):
         self.name = name
@@ -11,9 +12,11 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
-        def add_courses(self, course_name):
-            self.finished_course.append(course_name)
+# зачем тебе такая функция? Ее нет в задании. Мы вручную заполняем список курсов.
+    def add_courses(self, course_name):
+        self.finished_course.append(course_name)
 
+# твоя функция для выставления оценок
     def grades_hw(self, lecturer, course, grade):
         if isinstance(lecturer, Lecturer) and course in lecturer.courses_attached and course in lecturer.courses_in_progress:
             if course in lecturer.grades:
@@ -23,21 +26,50 @@ class Student:
         else:
             return 'Ошибка'
 
+# моя функция для выставления оценок лекторам.
+    def rating_lecturer(self, lecturer, course, grade):
+        """ Implement the method for the grading of the lecturers """
+        if 0 <= grade <= 10:  # необязательное условие - можно удалить этот блок if/else
+            # if isinstance проверка на соответствие: lecturer, Lecturer просто проверяет есть ли вообще такой лектор
+            # course in self.courses_in_progress есть ли курс у студента. Progress в смысле не завершено.
+            # course in lecturer.courses_attached есть ли у лектора такой предмет для чтения.
+            if isinstance(lecturer,
+                          Lecturer) and course in self.courses_in_progress and course in lecturer.courses_attached:
+                # есть ли курс
+                if course in lecturer.rating_grades:  # lecturer. <-- это обращение к классу. Словарь искать там.
+                    lecturer.rating_grades[course] += [grade]  # тут понятно? Это добавление в словарь. Поищи как
+                    # правильно добавить ['ключ', 'значение'] и сразу текст обретет смысл.
+                    # lecturer. <-- это ссылка на хранилище словаря. В класс Lecturer. rating_grade - название
+                    # [course] это 'key' += [grade] это наша новая оценка лектору от студента (от 1 до 10).
+                else:
+                    lecturer.rating_grades[course] = [grade]  # инициализация первой оценки т.к. в самом начале оценок
+                    # нет. Первая оценка проскакивает мимо if (пусто) сразу на else. Затем if сверяет со списком а
+                    #  += [grade] добавляет к существующему.
+            else:
+                return 'Error'
+        else:
+            return 'Wrong grade, only 0 to 10'
+
+
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
         self.courses_attached = []
 
+
 class Lecturer(Mentor):
-     def __init__(self):
-         self.grades = []
-         self.courses_in_progress = []
+    def __init__(self, name, surname):
+        super().__init__(name, surname)  # добавили метод super для связки с родителем
+        self.grades = []
+        self.courses_in_progress = []
+        self.rating_grades = {}  # создали словарь для записи оценок. Именно словарь! {'key', 'value'} дальше будем
+        # искать нужные данные по ключу из словаря.
 
 
 class Reviewer(Mentor):
-    """ выставлять студентам оценки за домашние задания """
     def rate_hw(self, student, course, grade):
+        """ выставлять студентам оценки за домашние задания """  # так положено, для обьяснения действий внутри
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
             if course in student.grades:
                 student.grades[course] += [grade]
